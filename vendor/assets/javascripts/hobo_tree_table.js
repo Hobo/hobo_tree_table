dcdfec3c4c94931610143d0d384bcce5fbdf7dc6
Bryan Larsen <bryan@larsen.st>
2012-03-30 16:25:36 -0400

    add reveal option

--- vendor/assets/javascripts/hobo_tree_table.js
+++ vendor/assets/javascripts/hobo_tree_table.js
@@ -1,5 +1,8 @@
 //= require_tree .
 
 jQuery.fn.hjq_tree_table = function(annotations) {
-     this.treeTable(this.hjq('getOptions', annotations));
+  this.treeTable(this.hjq('getOptions', annotations));
+  if(annotations.reveal) {
+    this.find("#"+annotations.reveal).reveal();
+  }
 };
