e1aa36974e3e2fbdeaf355e8ba9f10400a90b2c2
Bryan Larsen <bryan@larsen.st>
2012-03-29 21:34:23 -0400

    fix EDIT_LINK_BASE

--- lib/hobo_tree_table.rb
+++ lib/hobo_tree_table.rb
@@ -3,7 +3,7 @@ module HoboTreeTable
   @@root = Pathname.new File.expand_path('../..', __FILE__)
   def self.root; @@root; end
 
-  EDIT_LINK_BASE = "https://github.com/bryanlarsen/hobo_tree_table/edit/master"
+  EDIT_LINK_BASE = "https://github.com/Hobo/hobo_tree_table/edit/master"
 
   if defined?(Rails)
     require 'hobo_tree_table/railtie'
